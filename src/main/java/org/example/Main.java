3750114e8f51d36b810ad218334f43ef6718c9e4
Shubha Dhang <shubhadhang@gmail.com>
2023-07-06 14:23:17 +0200

    Calculator program without using switch statement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,45 +3,107 @@ package org.example;
 
 import java.util.Scanner;
 
+
 public class Main {
-    double add;
-    void Add(double n1, double n2)
+
+
+    /*public static void Add(double n1, double n2)
+     {
+         double add;
+         add=n1+n2;
+         System.out.println("Addition of two numbers:" +add);
+
+     }
+
+     public static void Sub(double n1, double n2) {
+         double sub;
+         System.out.println("Subtraction of two numbers:");
+         sub = n1 - n2;
+         System.out.println("Subtraction of two numbers:" +sub);
+     }
+
+     public static void  Mul(double n1, double n2) {
+         double mul;
+         System.out.println("MULTIPLICATION");
+         mul = n1 * n2;
+         System.out.println("Multiplication of two numbers:" +mul);
+
+     }
+
+     public static void Div(double n1, double n2) {
+         double div=0;
+         try {
+             div = n1 / n2;
+             //System.out.println("Division of two numbers:" + div);
+         }
+         catch (ArithmeticException ae)
+         {
+             System.out.println("ArithmeticException--"+ae.getMessage());
+         }
+         System.out.println("Division of two numbers:"+div);
+     }*/
+    public static void Addition() {
+
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("First num :");
+        double n1=scanner.nextDouble();
+        System.out.println("second number:");
+        double n2=scanner.nextDouble();
+        double add = n1 + n2;
+        System.out.println("add" + add);
+    }
+    public static void Subtraction() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("First num :");
+        double n1 = scanner.nextDouble();
+        System.out.println("second number:");
+        double n2 = scanner.nextDouble();
+        double sub = n1 - n2;
+        System.out.println(" Sub  :  " + sub);
+    }
+    public static void Multiplication()
     {
-        add=n1+n2;
-        System.out.println("Addition of two numbers:" +add);
+        Scanner  scanner = new Scanner(System.in);
+        System.out.println("First num :");
+        double n1=scanner.nextDouble();
+        System.out.println("second number:");
+        double n2=scanner.nextDouble();
+        double multi=n1*n2;
+        System.out.println("Multiplication  : "+multi);
+    }
+    public static void Division()
+    {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("First num :");
+        double n1=scanner.nextDouble();
+        System.out.println("second number:");
+        double n2=scanner.nextDouble();
 
-    }
-    double sub;
-    public void Sub(double n1, double n2) {
-        System.out.println("Subtraction of two numbers:");
-        sub = n1 - n2;
-        System.out.println("Subtraction of two numbers:" +sub);
-    }
-    double mul;
-    public void  Mul(double n1, double n2) {
-        System.out.println("MULTIPLICATION");
-        mul = n1 * n2;
-        System.out.println("Multiplication of two numbers:" +mul);
-
-    }
-    double div;
-    public void Div(double n1, double n2) {
         try {
-            div = n1 / n2;
-            //System.out.println("Division of two numbers:" + div);
+                double div= n1/n2;
+            System.out.println("Division  : "+div);
         }
         catch (ArithmeticException ae)
         {
-            System.out.println("ArithmeticException--"+ae.getMessage());
+            System.out.println(ae.getMessage());
         }
-        System.out.println("Division of two numbers:"+div);
     }
+       /* public static void EndCal(boolean b)
+        {
+            while(b);
+        }*/
+        public static void defaultBlock()
+        {
+
+            System.out.println("You have choose wrong option...");
+        }
+
 
 
     public static void main(String[] args) {
 
 
-        double n1, n2;
+       /* double n1, n2;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hi welcome  simple calculator....!!");
@@ -56,7 +118,7 @@ public class Main {
             System.out.println("5-exit");
             System.out.println("-----------------------------------------------------------------------");
             int num = scanner.nextInt();
-            Main ad=new Main();
+
             switch (num) {
                 case 1:
                 {
@@ -65,7 +127,7 @@ public class Main {
                     n1 = scanner.nextInt();
                     System.out.println("enter second number:");
                     n2 = scanner.nextInt();
-                    ad.Add(n1,n2);
+                    Add(n1,n2);
                     break;
                 }
                 case 2:
@@ -75,7 +137,7 @@ public class Main {
                     n1 = scanner.nextInt();
                     System.out.println("enter second number:");
                     n2 = scanner.nextInt();
-                        ad.Sub(n1, n2);
+                    Sub(n1, n2);
                         break;
                 }
                 case 3:
@@ -85,7 +147,7 @@ public class Main {
                     n1 = scanner.nextInt();
                     System.out.println("enter second number:");
                     n2 = scanner.nextInt();
-                    ad.Mul(n1, n2);
+                    Mul(n1, n2);
                     break;
                 }
                 case 4:
@@ -95,7 +157,7 @@ public class Main {
                     n1 = scanner.nextInt();
                     System.out.println("enter second number:");
                     n2 = scanner.nextInt();
-                    ad.Div(n1,n2);
+                    Div(n1,n2);
                     break;
                 }
                 case 5:
@@ -110,8 +172,37 @@ public class Main {
             }
         }
         while (y);
+    }*/
+        Scanner sc=new Scanner(System.in);
+
+        boolean s= true;
+
+       do
+           {
+               System.out.println("Addition 1");
+               System.out.println("sub 2");
+               System.out.println("Multi 3");
+               System.out.println("div 4");
+               System.out.println("exit 5");
+               int num=sc.nextInt();
+
+               if(num==1)
+               {
+                   Addition();
+               }
+               else if (num==2) { Subtraction();}
+               else if (num==3) { Multiplication();}
+               else if (num==4) {  Division();}
+               else if (num==5) {  s= false; break;}
+
+                else {
+                   defaultBlock();
+               }
+
+               } while (s);
+           }
+
+
+
     }
 
-
-
-}
