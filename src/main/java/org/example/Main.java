f268d794a1d60a5840c295b627261bbe9f6481ca
Shubha Dhang <shubhadhang@gmail.com>
2023-07-06 14:27:48 +0200

    Calculator program without using switch statement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,41 +7,8 @@ import java.util.Scanner;
 public class Main {
 
 
-    /*public static void Add(double n1, double n2)
-     {
-         double add;
-         add=n1+n2;
-         System.out.println("Addition of two numbers:" +add);
 
-     }
 
-     public static void Sub(double n1, double n2) {
-         double sub;
-         System.out.println("Subtraction of two numbers:");
-         sub = n1 - n2;
-         System.out.println("Subtraction of two numbers:" +sub);
-     }
-
-     public static void  Mul(double n1, double n2) {
-         double mul;
-         System.out.println("MULTIPLICATION");
-         mul = n1 * n2;
-         System.out.println("Multiplication of two numbers:" +mul);
-
-     }
-
-     public static void Div(double n1, double n2) {
-         double div=0;
-         try {
-             div = n1 / n2;
-             //System.out.println("Division of two numbers:" + div);
-         }
-         catch (ArithmeticException ae)
-         {
-             System.out.println("ArithmeticException--"+ae.getMessage());
-         }
-         System.out.println("Division of two numbers:"+div);
-     }*/
     public static void Addition() {
 
         Scanner scanner= new Scanner(System.in);
@@ -88,10 +55,7 @@ public class Main {
             System.out.println(ae.getMessage());
         }
     }
-       /* public static void EndCal(boolean b)
-        {
-            while(b);
-        }*/
+
         public static void defaultBlock()
         {
 
@@ -102,77 +66,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-       /* double n1, n2;
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Hi welcome  simple calculator....!!");
-        boolean y = true;
-        do {
-            System.out.println("You now will get options please select one to proceed further..");
-            System.out.println("----------------------------------------------------------------------");
-            System.out.println("1-ADDITION");
-            System.out.println("2-SUBTRACTION");
-            System.out.println("3-MULTIPLICATION");
-            System.out.println("4-DIVISION");
-            System.out.println("5-exit");
-            System.out.println("-----------------------------------------------------------------------");
-            int num = scanner.nextInt();
-
-            switch (num) {
-                case 1:
-                {
-                    System.out.println("---ADDITION---");
-                    System.out.println("enter first number:");
-                    n1 = scanner.nextInt();
-                    System.out.println("enter second number:");
-                    n2 = scanner.nextInt();
-                    Add(n1,n2);
-                    break;
-                }
-                case 2:
-                {
-                        System.out.println("---SUBTRACTION---");
-                    System.out.println("enter first number:");
-                    n1 = scanner.nextInt();
-                    System.out.println("enter second number:");
-                    n2 = scanner.nextInt();
-                    Sub(n1, n2);
-                        break;
-                }
-                case 3:
-                {
-                    System.out.println("---MULTIPLICATION---");
-                    System.out.println("enter first number:");
-                    n1 = scanner.nextInt();
-                    System.out.println("enter second number:");
-                    n2 = scanner.nextInt();
-                    Mul(n1, n2);
-                    break;
-                }
-                case 4:
-                {
-                   System.out.println("---DIVISION---");
-                    System.out.println("enter first number:");
-                    n1 = scanner.nextInt();
-                    System.out.println("enter second number:");
-                    n2 = scanner.nextInt();
-                    Div(n1,n2);
-                    break;
-                }
-                case 5:
-                {
-                    y=false;
-                    break;
-                }
-                default:
-                    System.out.println("you have choose the wrong option");
-
-
-            }
-        }
-        while (y);
-    }*/
         Scanner sc=new Scanner(System.in);
 
         boolean s= true;
